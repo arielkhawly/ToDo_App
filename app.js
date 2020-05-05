a9b9db78431d6a4b3609de979a92e98a3e453ca5
Ariel Khawly <akhawly59@gmail.com>
2020-05-05 15:57:14 -0500

    added close button to note form, starting functionality for closing and opening form

--- app.js
+++ app.js
@@ -2,7 +2,8 @@ class App {
     constructor() {
         this.$form = document.querySelector('#form')
         this.$noteTitle = document.querySelector('#note-title')
-        this.formButton = document.querySelector('#post-button')
+        this.$noteBody = document.querySelector('#note-body')
+        this.$formButton = document.querySelector('#post-button')
 
         this.addEventListeners();
     }
@@ -10,10 +11,16 @@ class App {
         document.body.addEventListener('click', event => {
             this.handleFormClick(event);
         });
-        this.$form.addEventListener('click', event => {
+        this.$formButton.addEventListener('submit', event => {
             event.preventDefault
             console.log('clicked!')
         })
+    }
+    handleFormClick() {
+        const isFormClicked = this.$form.contains(event.target);
+        if (isFormClicked) {
+            
+        }
     }
 }
 
