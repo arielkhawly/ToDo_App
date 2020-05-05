84182353158aeefe7f032faf5c4f9b054e227527
Ariel Khawly <akhawly59@gmail.com>
2020-05-05 17:02:29 -0500

    close button now functional and separate from open functionality, still working on displaying open button. creating new note object

--- app.js
+++ app.js
@@ -2,10 +2,13 @@ class App {
     constructor() {
         this.notes = [];
 
-        this.$form = document.querySelector('#form')
-        this.$noteTitle = document.querySelector('#note-title')
-        this.$noteBody = document.querySelector('#note-body')
-        this.$formButtons = document.querySelector('#form-buttons')
+        this.$form = document.querySelector('#form');
+        this.$noteTitle = document.querySelector('#note-title');
+        this.$noteBody = document.querySelector('#note-body');
+        this.$postButton = document.querySelector('#post-button')
+        this.$closeButton = document.querySelector('#close-button')
+
+       
 
         this.addEventListeners();
     }
@@ -14,28 +17,34 @@ class App {
             this.handleFormClick(event);
 
         });
-        this.$formButtons.addEventListener('submit', event => {
+        this.$postButton.addEventListener('submit', event => {
             event.preventDefault()
             console.log('clicked!')
-            this.addNote()
+            // this.addNote()
         })
-       addNote(note) {
-           const newNote = {
-            
-           }
-          
-       }
+        //    addNote(note) {
+        //        const newNote = {
+                    const title = this.$noteTitle.value
+                    const body = this.$noteBody.value
+                    const hasNote = 
+                    if (hasNote)
+
+        //        }
+
     }
     handleFormClick() {
-        const isFormClicked = this.$form.contains(event.target);
+        const isFormClicked = this.$postButton.contains(event.target);
         if (isFormClicked) {
             this.openForm()
         }
         else {
             this.closeForm()
+          
+
         }
     }
     openForm() {
+        console.log('form open')
         this.$form.classList.add('form-open')
         this.$noteTitle.style.display = 'block';
         this.$noteBody.style.display = 'block';
@@ -43,8 +52,15 @@ class App {
     closeForm() {
         this.$form.classList.remove('form-open')
         this.$noteTitle.style.display = 'none';
-        this.$formButtons.style.display = 'none';
-}
+        this.$noteBody.style.display = 'none'
+        this.$closeButton.style.display = 'none';
+        let openButton = document.createElement('BUTTON');
+        let openText = document.createTextNode('Open');
+        openButton.appendChild(openText);
+        document.body.appendChild(openButton)
+     
+        console.log('form closed')
+    }
 
 }
 new App()
