34b791ed3b9b86229dffbd06a613c921e913a977
Ariel Khawly <akhawly59@gmail.com>
2020-05-05 16:20:22 -0500

    buttons handling events, working on new note posting

--- app.js
+++ app.js
@@ -1,27 +1,50 @@
 class App {
     constructor() {
+        this.notes = [];
+
         this.$form = document.querySelector('#form')
         this.$noteTitle = document.querySelector('#note-title')
         this.$noteBody = document.querySelector('#note-body')
-        this.$formButton = document.querySelector('#post-button')
+        this.$formButtons = document.querySelector('#form-buttons')
 
         this.addEventListeners();
     }
     addEventListeners() {
         document.body.addEventListener('click', event => {
             this.handleFormClick(event);
+
         });
-        this.$formButton.addEventListener('submit', event => {
-            event.preventDefault
+        this.$formButtons.addEventListener('submit', event => {
+            event.preventDefault()
             console.log('clicked!')
+            this.addNote()
         })
+       addNote(note) {
+           const newNote = {
+            
+           }
+          
+       }
     }
     handleFormClick() {
         const isFormClicked = this.$form.contains(event.target);
         if (isFormClicked) {
-            
+            this.openForm()
+        }
+        else {
+            this.closeForm()
         }
     }
+    openForm() {
+        this.$form.classList.add('form-open')
+        this.$noteTitle.style.display = 'block';
+        this.$noteBody.style.display = 'block';
+    }
+    closeForm() {
+        this.$form.classList.remove('form-open')
+        this.$noteTitle.style.display = 'none';
+        this.$formButtons.style.display = 'none';
 }
 
+}
 new App()
